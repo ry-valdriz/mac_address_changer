049f2dd0c5e8f4ed0b4ee844b3490efa7f54b66a
ry-valdriz <ryvaldriz101@gmail.com>
2020-01-14 01:16:06 -0500

    command line parser

--- mac_changer.py
+++ mac_changer.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse as op
+
+#command line arguments
+parser = op.OptionParser()
+parser.add_option("-i", "--interface", dest = "interface", help = "Interface that will have its MAC address changed")
+parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 # interface = "eth0"
-# original_mac = "08:00:27:5b:b1:a6"
+# new_mac = "08:00:27:5b:b1:a6" #original
 # new_mac = "00:11:22:33:44:55"
 
-interface = input("interface: ")
-new_mac = input("new mac(xx:xx:xx:xx:xx:xx): ")
+#select interface and new mac address
+#interface = input("interface: ")
+#new_mac = input("new mac(xx:xx:xx:xx:xx:xx): ")
 
 print("[+] Changing Mac address for " + interface)
-print("[+] new MAC address: " + new_mac)
+print("[+] New MAC address: " + new_mac)
 
 # subprocess.call("ifconfig eth0 down", shell = True)
 # subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell = True)
