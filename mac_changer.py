fd10a4654519a573122e84e5303728e809725c4d
ry-valdriz <ryvaldriz101@gmail.com>
2020-01-14 01:26:56 -0500

    clean code

--- mac_changer.py
+++ mac_changer.py
@@ -3,31 +3,28 @@
 import subprocess
 import optparse as op
 
-#command line arguments
-parser = op.OptionParser()
-parser.add_option("-i", "--interface", dest = "interface", help = "Interface that will have its MAC address changed")
-parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
+def change_mac(interface, new_mac):
+    # interface = "eth0"
+    # new_mac = "08:00:27:5b:b1:a6" #original
+    # new_mac = "00:11:22:33:44:55"
 
-(options, arguments) = parser.parse_args()
+    print("[+] Changing Mac address for " + interface)
+    print("[+] New MAC address: " + new_mac)
 
-interface = options.interface
-new_mac = options.new_mac
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-# interface = "eth0"
-# new_mac = "08:00:27:5b:b1:a6" #original
-# new_mac = "00:11:22:33:44:55"
+def terminal_input():
+    #command line arguments
+    parser = op.OptionParser()
+    parser.add_option("-i", "--interface", dest = "interface", help = "Interface that will have its MAC address changed")
+    parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
+    return parser.parse_args()
 
-#select interface and new mac address
-#interface = input("interface: ")
-#new_mac = input("new mac(xx:xx:xx:xx:xx:xx): ")
+(options, arguments) = terminal_input()
 
-print("[+] Changing Mac address for " + interface)
-print("[+] New MAC address: " + new_mac)
+change_mac(options.interface, options.new_mac)
 
-# subprocess.call("ifconfig eth0 down", shell = True)
-# subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell = True)
-# subprocess.call("ifconfig eth0 up", shell = True)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+
