344540367683d0d6ce0a6ebdeae5cad3781620d6
ry-valdriz <ryvaldriz101@gmail.com>
2020-01-14 01:38:57 -0500

    argument checking

--- mac_changer.py
+++ mac_changer.py
@@ -15,15 +15,22 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-def terminal_input():
+def get_arguments():
     #command line arguments
     parser = op.OptionParser()
     parser.add_option("-i", "--interface", dest = "interface", help = "Interface that will have its MAC address changed")
     parser.add_option("-m", "--mac", dest = "new_mac", help = "New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    
+    if not options.interface:
+        parser.error("[-] Please specify an interface to use, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC address to use, use --help for more info")
 
-(options, arguments) = terminal_input()
+    return options
 
+
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
 
 
